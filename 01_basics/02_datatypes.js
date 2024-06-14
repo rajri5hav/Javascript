ef1537e1091e6d3885eca31fe6d93940d4829899
rajri5hav <150959698+rajri5hav@users.noreply.github.com>
2024-06-14 20:27:38 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+
+    console.log("rishav")
+
+
+    let name = "rishav"
+    let age = 20
+    let isLoggedIn = "false"
+    let state;
+
+
+    // number => 2 to power 53
+    // bigint
+    //string => ""
+    // boolean => standalone value
+    // undefined
+    // symbol => uniquess
+
+
+    // object
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
