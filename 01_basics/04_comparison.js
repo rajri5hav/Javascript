aa4c4d8e4fde70aff7a451786755feba01e29637
rajri5hav <150959698+rajri5hav@users.noreply.github.com>
2024-06-15 11:38:46 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);     // before comparing any values make sure that there datatypes must be same.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);           // clean code is most valuable. This type of comparison create confusion any time , so avoid this (fro 11 to 17). 
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
