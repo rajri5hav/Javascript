9d92bceb8401454a6f618edacd6a6381edcc7d4d
rajri5hav <150959698+rajri5hav@users.noreply.github.com>
2024-06-15 11:17:53 +0000

    conversion and operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,66 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Rishav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "rishav" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//****************************  Operations *************************
+
+ let value = 3
+ let negValue = -value
+ //console.log(negValue);
+
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2**3);
+ //console.log(2/3);
+ //console.log(2%3);
+
+ let str1 = "hello"
+ let str2 = " rishav"
+
+ let str3 = str1 + str2
+ console.log(str3);
+
+//  console.log("1" + 2);
+//  console.log(1 + "2");
+//  console.log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+
+//  console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");   // do not write this type of code (from 54 to 60)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2   // beacaue readability is important
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// I commented some code to run one code at a time for understanding it better.
